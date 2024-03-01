998077dc04554ec00b22d253f2f0a04f66e469eb
leall <unifamussio@fei.edu.br>
2024-03-01 23:16:48 +0000

    uiui aplication

--- teste.c
+++ teste.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
+#include "uiui.h"
 
 int main(){
-    int num;
-    printf("Digite um número e eu adivinharei: ");
-    scanf("%d", &num);
-    printf("numero digitado: %d\n", num);
+    adivinhei();
 }
---
+++ uiui.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void adivinhei(){
+    int num;
+    printf("Digite um número e eu adivinharei: ");
+    scanf("%d", &num);
+    printf("numero digitado: %d\n", num);
+}
